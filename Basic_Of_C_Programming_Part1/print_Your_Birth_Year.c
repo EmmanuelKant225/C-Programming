8accd45cdf5a7551c0390324ada9469851c7feaa
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:54:01 +0100

    Write a program that prints the year you were born.

---
+++ Basic_Of_C_Programming_Part1/print_Your_Birth_Year.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Write a program that prints the year you were born. 
+ * Include in the printing line the substraction calculation between the 'current year' and 'your age'
+ */
+
+int main()
+{
+	printf("The year you were born: %d\n", 2022-28);
+
+	return 0;
+}
