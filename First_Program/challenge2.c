1556db265c3472b3c61e178380eddd6af857ca4e
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:13:59 +0100

    Write a program that will print your Full Name, Age and Gender in different line,

---
+++ First_Program/challenge2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	// Write a that will print your Full Name, Age and Gender in different line.
+	
+	printf("Emanuel Adigra\n");
+	printf("28 Years Old\n");
+	printf("Male");
+
+	return 0;
+}
+
