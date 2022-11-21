95ae329f8d9538b6edfa510038b1422b8a910ea8
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:38:42 +0100

    Write a program that prints a pattern

---
+++ Basic_Of_C_Programming_Part1/print_Astericks_Patten.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Write a program that prints the follwoing Pattern
+// 1*********
+// 12********
+// 123*******
+// 1234******
+// 12345*****
+
+
+
+int main()
+{
+	printf("1*********\n12********\n123*******\n1234******\n12345*****\n");
+
+	return 0;
+}
