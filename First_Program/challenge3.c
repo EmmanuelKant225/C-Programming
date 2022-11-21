b6905ab395b7146d950a74b6f278266c6dabd58c
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:23:59 +0100

    Write a program that will print an Isosceles Triangle with * using multiple printf or only one printf

---
+++ First_Program/challenge3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	/* Write a program that will print an Isosceles Triangle with * using multiple printf or only one printf */
+	
+	/* First Option with multiple printf*/
+	
+	printf("  *  \n");
+	printf(" *** \n");
+	printf("*****\n");
+	
+	/* Second option will only one printf*/
+	
+	printf("  *  \n *** \n*****\n");
+	return 0;
+
+}
