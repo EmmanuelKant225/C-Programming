0ee11b581c2a036765c5a3e4fbacf0435af9f909
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:33:17 +0100

    Write a program that prints an Rectangle Astericks

---
+++ Basic_Of_C_Programming_Part1/README.md
@@ -0,0 +1,1 @@
+This Part 1 of Basic of C Programming concerns the use of the printf() function to print certain forms
---
+++ Basic_Of_C_Programming_Part1/print_Rectangle_Of_Astericks.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	/* Write a program that prints an Astericks rectangle*/
+
+	printf("**********\n**********\n**********\n**********\n**********\n");
+
+	return 0;
+}
+
