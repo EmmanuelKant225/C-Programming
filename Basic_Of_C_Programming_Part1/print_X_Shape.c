f1102f48ddbc2966a7ff50706d607f1ea8357a1d
Emmanuel ADIGRA <systemengineer225@yahoo.com>
2022-11-21 19:47:20 +0100

    Write a program that prints a X Shape

---
+++ Basic_Of_C_Programming_Part1/print_X_Shape.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Write a program that prints the follwoing X shape
+ * 	*     *
+ * 	 *   *
+ * 	  * *
+ * 	   *
+ * 	  * *
+ * 	 *   *
+ * 	*     *
+ * */
+
+
+
+int main()
+{
+	printf("*     *\n");
+	printf(" *   *\n");
+	printf("  * *\n");
+	printf("   *\n");
+	printf("  * *\n");
+	printf(" *   *\n");
+	printf("*     *\n");
+
+	return 0;
+}
